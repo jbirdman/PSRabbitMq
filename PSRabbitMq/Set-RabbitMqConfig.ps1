47734819e96665814a0ea16a56fca9e40e8a8340
Jeffrey Bird <jeffrey.bird@jcu.edu.au>
2015-10-08 14:42:35 +1000

    Store URI in RabbitMqConfig.

--- PSRabbitMq/Set-RabbitMqConfig.ps1
+++ PSRabbitMq/Set-RabbitMqConfig.ps1
@@ -11,6 +11,9 @@
     .PARAMETER ComputerName
         Specify a ComputerName to use
 
+    .PARAMETER Uri
+        Specify a URI to use
+
     .Example
         Set-RabbitMqConfig -ComputerName "rabbitmq.contoso.com"
 
@@ -20,6 +23,7 @@
     [cmdletbinding()]
     param(
         [string]$ComputerName
+        [string]$Uri
     )
 
     #handle PS2
@@ -31,6 +35,11 @@
     If($PSBoundParameters.ContainsKey('ComputerName'))
     {
         $Script:RabbitMqConfig.ComputerName = $ComputerName
+    }
+
+    If($PSBoundParameters.ContainsKey('Uri'))
+    {
+        $Script:RabbitMqConfig.Uri = $Uri
     }
 
     $Script:RabbitMqConfig | Export-Clixml -Path "$PSScriptRoot\PSRabbitMq.xml" -force
